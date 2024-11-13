8f0320ddd1310e7a2c49803d7f9514c87c240ff8
Davi Marques <davimgcb@gmail.com>
2024-11-13 10:19:57 -0300

    feat: theme type

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Button variant="primary"/>
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary"/>
+      </ThemeProvider>
     </>
   )
 }
