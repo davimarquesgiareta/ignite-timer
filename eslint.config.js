19cff04d4b7c5142d3e78d9c36e7dae836868d98
Davi Marques <davimgcb@gmail.com>
2024-11-17 16:40:57 -0300

    feat: styles and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.ts", "**/*.tsx"], // Apenas arquivos TypeScript
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -22,6 +22,14 @@ export default tseslint.config(
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true, // Permite expressões de curto-circuito (como `a && b`)
+          allowTernary: true,      // Permite expressões ternárias (como `a ? b : c`)
+          allowTaggedTemplates: true, // Permite templates taggeados
+        },
       ],
     },
   },
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
@@ -6,23 +6,12 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
-const buttonVariants = {
-  primary: 'purple',
-  secondary: 'orange',
-  danger: 'red',
-  success: 'green'
-}
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  background-color: ${props => props.theme.primary};
-  color: ${props => props.theme.white}
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
 
-  /* ${props => {
-    return css`
-      background-color :${buttonVariants[props.variant]}
-    `
-  } } */
+  cursor: pointer;
 `
